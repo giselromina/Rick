19786cd6ab200617f78563b7f06a3ac97d417cdd
giselromina <giselromina@gmail.com>
2020-03-16 17:21:58 +0100

    created filtered by getters and setter

--- src/app/shared/shared-list/shared-list.component.ts
+++ src/app/shared/shared-list/shared-list.component.ts
@@ -7,26 +7,45 @@ import { DataTableCharacter } from '../models/dataTableCharacter';
   styleUrls: ['./shared-list.component.css']
 })
 export class SharedListComponent implements OnInit {
+
+
+ private _listfilterGS: string;
+
+  get listfilterGS() {
+    return this._listfilterGS;
+  }
+
+  set listfilterGS(filter: string) {
+    this._listfilterGS = filter;
+    this.applyFilter(this.listfilterGS);
+  }
+
   constructor() { }
   @BlockUI() blockUI: NgBlockUI;
   @Input() dataSource: DataTableCharacter[];
   displayedColumns: string[];
   listfilter: string;
   filteredCharacters: DataTableCharacter[];
+
   ngOnInit() {
     this.displayedColumns = Object.keys(this.dataSource[0]);
     this.filteredCharacters = this.dataSource;
   }
 
-  applyFilter(event?: Event): void {
-    if (event) {
+filteredBy(event?: Event) {
       const filterValue = (event.target as HTMLInputElement).value;
+      this.applyFilter(filterValue);
+}
+
+  applyFilter(key?: string|null): void {
+    if (key) {
       this.filteredCharacters = this.dataSource.filter(
-        (character: DataTableCharacter) => character.name.toLocaleLowerCase().indexOf(filterValue.toLocaleLowerCase()) !== -1);
+        (character: DataTableCharacter) => character.name.toLocaleLowerCase().indexOf(key.toLocaleLowerCase()) !== -1);
     } else {
       this.filteredCharacters = this.dataSource;
     }
   }
+
   toggleImag(btn) {
     console.log(btn);
   }
